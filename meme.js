6df8fd56df493ea8e464cf24b3c8699c633cff4b
Genna Mergola <gennamergola@gmail.com>
2020-02-13 07:30:20 -0800

    updating js

--- meme.js
+++ meme.js
@@ -11,46 +11,55 @@ document.body.onload = form.addEventListener("submit", function(event){
      // creating parent div
     let theDiv = document.createElement("div");
     theDiv.classList.add("the-div");
-
-    // creating image
-    var divImage = document.createElement("div");
-    var image = document.createElement("img"); 
-    image.classList.add("image");
-    // giving it some content 
-    image.src = imageURL.value;
-    image.width = "600";
-    image.height = "500";
-    // adding the text node to the newly created div
-    divImage.appendChild(image);
-    theDiv.appendChild(divImage);  
     
      
     // creating top text div
-    var topSpan = document.createElement("div"); 
+    var topSpan = document.createElement("span"); 
     topSpan.classList.add("top");
     // giving it some content 
     var textInside = document.createTextNode(topText.value); 
     // adding the text node to the newly created div
     topSpan.appendChild(textInside);  
     theDiv.appendChild(topSpan);
-    var memeGallery = document.body.insertBefore(theDiv, memeGallery); 
+    // var memeGallery = document.body.insertBefore(theDiv, memeGallery); 
 
     
-    
     // creating bottom text div
-    var bottomSpan = document.createElement("div"); 
+    var bottomSpan = document.createElement("span"); 
     bottomSpan.classList.add("bottom");
     // giving it some content 
     var bottomInside = document.createTextNode(bottomText.value); 
     // adding the text node to the newly created div
     bottomSpan.appendChild(bottomInside);  
     theDiv.appendChild(bottomSpan);
-    var memeGallery = document.body.insertBefore(theDiv, memeGallery); 
 
-    gallery.appendChild(theDiv);
+    // creating image
+    // var divImage = document.createElement("div");
+    var image = document.createElement("img"); 
+    image.classList.add("image");
+    // giving it some content 
+    image.src = imageURL.value;
+    image.width = "600";
+    image.height = "500";
+    image.click();
+    // adding the text node to the newly created div
+    // divImage.appendChild(image);
+    theDiv.appendChild(image);  
+  
+
+    // adding the meme to the page
+    var biggestDiv = document.createElement("div");
+    biggestDiv.appendChild(theDiv);
+    gallery.appendChild(biggestDiv);
     form.reset();
 
+    biggestDiv.addEventListener("click", function(){
+        biggestDiv.removeChild(theDiv);
+    });
+
 });
+
+
 
 
 
