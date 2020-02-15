0fdda48e3900520111644000da59ac5b5f90fd46
Genna Mergola <gennamergola@gmail.com>
2020-02-14 16:39:08 -0800

    updating js

--- meme.js
+++ meme.js
@@ -5,7 +5,7 @@ var bottomText = document.getElementById("textonbottom");
 var imageURL = document.getElementById("imglink");
 var gallery = document.getElementById("gallery");
 
-document.body.onload = form.addEventListener("submit", function(event){
+form.addEventListener("submit", function(event){
     event.preventDefault();
 
      // creating parent div
@@ -21,7 +21,6 @@ document.body.onload = form.addEventListener("submit", function(event){
     // adding the text node to the newly created div
     topSpan.appendChild(textInside);  
     theDiv.appendChild(topSpan);
-    // var memeGallery = document.body.insertBefore(theDiv, memeGallery); 
 
     
     // creating bottom text div
@@ -34,25 +33,23 @@ document.body.onload = form.addEventListener("submit", function(event){
     theDiv.appendChild(bottomSpan);
 
     // creating image
-    // var divImage = document.createElement("div");
     var image = document.createElement("img"); 
     image.classList.add("image");
     // giving it some content 
     image.src = imageURL.value;
     image.width = "600";
     image.height = "500";
-    image.click();
-    // adding the text node to the newly created div
-    // divImage.appendChild(image);
     theDiv.appendChild(image);  
   
 
     // adding the meme to the page
     var biggestDiv = document.createElement("div");
+    biggestDiv.classList.add("biggest-div")
     biggestDiv.appendChild(theDiv);
     gallery.appendChild(biggestDiv);
     form.reset();
 
+    //removing each meme
     biggestDiv.addEventListener("click", function(){
         biggestDiv.removeChild(theDiv);
     });
